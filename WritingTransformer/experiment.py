1e73d8d67626669416bd3e05008508a7702e7b02
Don <dcbrien@gmail.com>
2022-11-20 13:40:25 -0500

    Starting an organized and maintainable version

---
+++ WritingTransformer/experiment.py
@@ -0,0 +1,3 @@
+# Don Nov 2022. The main entry point for the production version of the Writing Transformer.
+# I'm going try using the Google tools for this and follow their design patterns (i.e., 
+# absl, ml_collections, jaxline).
